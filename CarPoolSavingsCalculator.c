d9e812d17d0f567a95e51b00fbc7075d73439163
scensorECHO <xxartofwarxx@gmail.com>
2015-11-07 14:31:56 -0500

    Completed all output, variables still registering as undeclared?

--- CarPoolSavingsCalculator.c
+++ CarPoolSavingsCalculator.c
@@ -9,28 +9,33 @@ int main(void)
 {
 	/* setup variables for output to user*/
 	double miles;
-	double totalMiles;
+	double dailyMiles;
 	double pricePerGallon;
 	double milesPerGallon;
 	double dailyParkingFees;
 	double dailyTolls;
+	double totalCost;
+	
+	printf("How many miles do you travel to work?\nEnter a decimal: ");
+	scanf("%f",&miles);
+	printf("\nHow much does gas cost per gallon?\nEnter a decimal: ");
+	scanf("%f",&pricePerGallon);
+	printf("\nWhat is your cars gas mileage? (As miles per gallon)\nEnter a decimal: ");
+	scanf("%f",&milesPerGallon);
+	printf("\nAre there any parking fees?\nEnter a decimal: ");
+	scanf("%f",&dailyParkingFees);
+	printf("\nAre there any tolls?\nEnter a decimal: ");
+	scanf("%f",&dailyTolls);
 
-	char[] decPrompt = "\nEnter a decimal: ";
+	dailyMiles = miles * 2;
+	totalCost = miles*milesPerGallon*pricePerGallon + dailyParkingFees + dailyTolls;
 
-	printf("How many miles do you travel to work?%s",decPrompt);
-	scanf("%d",&miles);
-	totalMiles = miles * 2;
-	printf("\nHow much does gas cost per gallon?%s",decPrompt);
-	scanf("%d",&pricePerGallon);
-	printf("\nWhat is your cars gas mileage? (As miles per gallon),%s",decPrompt);
-	scanf("%d",&milesPerGallon);
-	printf("\nAre there any parking fees?%s",decPrompt);
-	scanf("%d",&dailyParkingFees);
-	printf("\nAre there any tolls?%s",decPrompt);
-	scanf("%d",&dailyTolls);
+	printf("\nTotal miles driven per day: %.2f",dailyMiles);
+	printf("\nCost per gallon of gasoline: %.2f",costPerGallon);
+	printf("\nAverage miles per gallon: &.2f",milesPerGallon);
+	printf("\nParking fees per day: %.2f",dailyParkingFees);
+	printf("\nTolls per day: %.2f",dailyTolls);
 
-
-
-
+	printf("\nTotal cost of commute to work: %.2f",totalCost);
 
 }
