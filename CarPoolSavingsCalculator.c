0717832dd4562b01160db13d93bcec6bcf71c487
scensorECHO <xxartofwarxx@gmail.com>
2015-11-07 13:37:54 -0500

    Created assignment 1

---
+++ CarPoolSavingsCalculator.c
@@ -0,0 +1,24 @@
+/*	Programmer Name: Tom Carrio
+	Program Purpose: To receive input from the user/s in order to 
+	calculate the cost savings of carpooling. 
+
+#include <stdio.h>
+
+int main(void)
+{
+	/* setup variables for output to user*/
+	double totalMiles;
+	double gasPerGallon;
+	double avgMilesPerGallon;
+	double dailyParkingFees;
+	double dailyTolls;
+
+	/* create variables for calculating per passenger*/
+	double milesByPassenger;
+	int totalPassengers;
+
+	printf("How many passengers will be carpooling with you?");
+	scanf(%d, $totalPassengers);
+
+	printf("")
+}
