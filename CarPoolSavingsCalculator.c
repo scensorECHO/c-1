4bbccc092481e9f93c4b2dbcb06a2041d18480f6
scensorECHO <xxartofwarxx@gmail.com>
2015-11-07 13:58:26 -0500

    Added prompts for variables

--- CarPoolSavingsCalculator.c
+++ CarPoolSavingsCalculator.c
@@ -2,23 +2,35 @@
 	Program Purpose: To receive input from the user/s in order to 
 	calculate the cost savings of carpooling. 
 */
+
 #include <stdio.h>
 
 int main(void)
 {
 	/* setup variables for output to user*/
+	double miles;
 	double totalMiles;
-	double gasPerGallon;
-	double avgMilesPerGallon;
+	double pricePerGallon;
+	double milesPerGallon;
 	double dailyParkingFees;
 	double dailyTolls;
 
-	/* create variables for calculating per passenger*/
-	double milesByPassenger;
-	int totalPassengers;
+	char[] decPrompt = "\nEnter a decimal: ";
 
-	printf("How many passengers will be carpooling with you?");
-	scanf(%d, $totalPassengers);
+	printf("How many miles do you travel to work?%s",decPrompt);
+	scanf("%d",&miles);
+	totalMiles = miles * 2;
+	printf("\nHow much does gas cost per gallon?%s",decPrompt);
+	scanf("%d",&pricePerGallon);
+	printf("\nWhat is your cars gas mileage? (As miles per gallon),%s",decPrompt);
+	scanf("%d",&milesPerGallon);
+	printf("\nAre there any parking fees?%s",decPrompt);
+	scanf("%d",&dailyParkingFees);
+	printf("\nAre there any tolls?%s",decPrompt);
+	scanf("%d",&dailyTolls);
 
-	printf("")
+
+
+
+
 }
